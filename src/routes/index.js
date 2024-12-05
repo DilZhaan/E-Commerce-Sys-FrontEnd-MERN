0e87746cc8b2b327df9082130d622336b443e416
ChamithDilshan <61306553+DilZhaan@users.noreply.github.com>
2024-12-06 01:31:01 +0530

    test CD

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
                 element : <SignUp/>
             },
             {
-                path : "/fogotPassword",
+                path : "/forgotpwd",
                 element : <ForgotPwd/>
             },
         ]
